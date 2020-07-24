9a838c888b78872e74ee2557d97dbb6d4d99cd8f
Justin Wang <junting.wang@thoughtworks.com>
2020-07-24 14:59:55 +0800

    Infra: optimize performance

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -2,20 +2,34 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/app.tsx',
   output: {
-    filename: '[name].[hash].js',
+    filename: 'js/[name].[contenthash].js',
     path: path.resolve(__dirname, '../dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+      chunkFilename: 'css/[id].[contenthash].css',
+    }),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
     hot: true,
     compress: true,
     historyApiFallback: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   module: {
     rules: [
@@ -27,7 +41,12 @@ module.exports = {
       {
         test: /\.(c|sa|sc)ss$/i,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
           {
             loader: 'css-loader',
             options: {
@@ -41,27 +60,44 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               ident: 'postcss',
-              plugins: [require('autoprefixer')]
-            }
+              plugins: [require('autoprefixer')],
+            },
           },
           'sass-loader',
         ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5 * 1024,
+              name: 'imgs/[name].[contenthash].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10 * 1024,
+              name: 'fonts/[name].[contenthash].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
   resolve: {
     extensions: ['.js', '.json', '.ts', '.tsx'],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
-  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 };
